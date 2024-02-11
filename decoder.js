84e33fa2875a1e8600ec6b24227b3fe28070ddcb
Joshua <105608879+Joshua-Repos@users.noreply.github.com>
2024-02-11 17:37:18 -0500

    Update decoder.js

--- decoder.js
+++ decoder.js
@@ -1,1 +1,17 @@
-
+var letter= prompt("what is th letter")
+switch (letter){
+  case "a":
+    { alert("b")
+     break
+     case "b":
+    alert("d")
+         break
+     case "c":
+    alert("f")     break
+     case "d":
+    alert("h")
+         break
+     case "e":
+    alert("j")
+    }
+}
